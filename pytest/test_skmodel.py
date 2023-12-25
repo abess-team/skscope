d368bc398eb96a0e95d6d44eef62a93033049320
chenpnn <chpeng2535@gmail.com>
2023-12-25 16:28:23 +0800

    Update test_skmodel.py

--- pytest/test_skmodel.py
+++ pytest/test_skmodel.py
@@ -21,14 +21,13 @@ import warnings
 warnings.filterwarnings("ignore")
 
 CURRENT = os.path.abspath(os.path.dirname(os.getcwd()))
-CURRENT = os.path.normpath(CURRENT)
 
 
 def test_PortfolioSelection():
     # load data
     port = PortfolioSelection(sparsity=50, alpha=0.001, random_state=0)
-    dir = os.path.normpath("../docs/source/gallery/Miscellaneous/data/csi500-2020-2021.csv")
-    X = pd.read_csv(dir, encoding="gbk")
+    dir = os.path.normpath("/docs/source/gallery/Miscellaneous/data/csi500-2020-2021.csv")
+    X = pd.read_csv(CURRENT+dir, encoding="gbk")
     keep_cols = X.columns[(X.isnull().sum() <= 20)]
     X = X[keep_cols]
     X = X.fillna(0)

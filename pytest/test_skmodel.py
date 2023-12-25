b4f782e872e0eea0fc72ce389de085684a818158
chenpnn <chpeng2535@gmail.com>
2023-12-25 16:36:04 +0800

    Update test_skmodel.py

--- pytest/test_skmodel.py
+++ pytest/test_skmodel.py
@@ -20,7 +20,7 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-CURRENT = os.path.abspath(os.path.dirname(os.getcwd()))
+CURRENT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 def test_PortfolioSelection():
